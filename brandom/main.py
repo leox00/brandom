b5a19d9904ea82da9f97d265e19115d35431bb2e
leox00 <146089334+leox00@users.noreply.github.com>
2024-04-07 17:38:20 +0200

    Added randstr function and fixes

--- brandom/main.py
+++ brandom/main.py
@@ -1,5 +1,14 @@
 from requests import get
 
+url = "https://www.random.org/integers/?num=1&min=1&max=10&col=5&base=10&format=plain&rnd=new"
+
 def randint(min: int = 1, max: int = 10, quantity: int = 1):
-    url = f"https://www.random.org/integers/?num={quantity}&min={min}&max={max}&col=5&base=10&format=plain&rnd=new"
-    print(get(url).text)
+    numbers = get("https://www.random.org/integers/?num={quantity}&min={min}&max={max}&col=5&base=10&format=plain&rnd=new").text
+    print(numbers)
+
+def randstr(lenght: int = 1, alphabet: str = "abcdefghijklmnopqrstuvwxyz"):
+    letters = ""
+    for i in range(lenght):
+        number = int(get(f"https://www.random.org/integers/?num=1&min=1&max={len(alphabet)-1}&col=5&base=10&format=plain&rnd=new").text)
+        letters += alphabet[number]
+    print(letters)
---
+++ brandom/test.py
@@ -0,0 +1,3 @@
+import main
+
+main.randstr(50)
