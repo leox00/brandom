79622cc002c810aa9dbb4832d01278181d8e1243
leox00 <146089334+leox00@users.noreply.github.com>
2024-04-07 23:15:15 +0200

    added ranbool

--- brandom/main.py
+++ brandom/main.py
@@ -3,8 +3,8 @@ from requests import get
 url = "https://www.random.org/integers/?num=1&min=1&max=10&col=5&base=10&format=plain&rnd=new"
 
 def randint(min: int = 1, max: int = 10, quantity: int = 1):
-    numbers = get(f"https://www.random.org/integers/?num={quantity}&min={min}&max={max}&col=5&base=10&format=plain&rnd=new").text
-    print(numbers)
+    number = get(f"https://www.random.org/integers/?num={quantity}&min={min}&max={max}&col=5&base=10&format=plain&rnd=new").text
+    print(number)
 
 def randstr(lenght: int = 1, alphabet: str = "abcdefghijklmnopqrstuvwxyz"):
     letters = ""
@@ -12,3 +12,13 @@ def randstr(lenght: int = 1, alphabet: str = "abcdefghijklmnopqrstuvwxyz"):
         number = int(get(f"https://www.random.org/integers/?num=1&min=1&max={len(alphabet)-1}&col=5&base=10&format=plain&rnd=new").text)
         letters += alphabet[number]
     print(letters)
+
+def randbool(quantity: int = 1):
+    for i in range(quantity):
+        number = int(get(f"https://www.random.org/integers/?num=1&min=0&max=1&col=5&base=10&format=plain&rnd=new").text)
+        if number == 0:
+            print(False)
+        elif number == 1:
+            print(True)
+        else:
+            raise ValueError(number)
--- brandom/test.py
+++ brandom/test.py
@@ -1,4 +1,5 @@
 import main
 
-main.randint()
-main.randstr()
+#main.randint()
+#main.randstr()
+main.randbool(2)
